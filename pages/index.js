6384fd27715a6365869e5fd3eec8cf2869a5b81e
Ike <ikescode@gmail.com>
2021-11-10 19:00:15 -0600

    add home content, add icon buttons, add resume

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Github, Instagram } from '@styled-icons/boxicons-logos'
+import { Envelope } from '@styled-icons/boxicons-solid'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-800 text-white">
       <Head>
         <title>Ike Holzmann</title>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
@@ -14,69 +18,38 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome!
-        </h1>
+        <Image src="/images/me-wave.png" width="100px" height="100px" />
+        <h1 className="text-6xl">Welcome to <span className="text-blue-400"><Link href="/">ike.dev</Link></span></h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+        <p className="mt-3 text-2xl animate-pulse text-gray-300">
+          My portfolio is under construction 👷
         </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className="mt-2 text-gray-200">
+          You can view my resume <a className="text-blue-400" href="/resume.pdf">here</a>.
+        </p>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-between w-64 h-24 text-blue-500">
         <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/ikejs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          <Github size="2em" color="white" />
+        </a>
+        <a
+          href="https://www.instagram.com/isaac.holzmann/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Instagram size="2em" color="white" />
+        </a>
+        <a
+          href="mailto:ike@holzmann.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Envelope size="2em" color="white" />
         </a>
       </footer>
     </div>
